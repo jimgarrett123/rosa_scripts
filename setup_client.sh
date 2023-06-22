3595f429e55ef43a40eff232137896022b8fddad
Jim Garrett <jimgarrett123@gmail.com>
2023-06-22 10:02:23 -0400

    Update setup_client.sh

--- setup_client.sh
+++ setup_client.sh
@@ -5,12 +5,8 @@ read name
 echo Please provide your ROSA Login Token
 read rosaToken
 
-echo Please provide your Scoarboard Server URL
-read serverUrl
-
 export MY_NAME="$name"
 export ROSA_OCM_TOKEN="$rosaToken"
-export SCOREBOARD_SERVER="$serverUrl"
 
 export ROSA_AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
 export ROSA_AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
