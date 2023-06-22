bc315ec9aaa4ab7a9003aaaa2d70d902200d000b
Jim Garrett <jimgarrett123@gmail.com>
2023-06-22 09:15:37 -0400

    Create setup_client.sh

---
+++ setup_client.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo What is your name?
+read name
+
+echo Please provide your ROSA Login Token
+read rosaToken
+
+echo Please provide your Scoarboard Server URL
+read serverUrl
+
+export MY_NAME="$name"
+export ROSA_OCM_TOKEN="$rosaToken"
+export SCOREBOARD_SERVER="$serverUrl"
+
+export ROSA_AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+export ROSA_AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+export ROSA_AWS_DEFAULT_REGION=$(aws configure get region)
