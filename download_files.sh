b8dc0e5fa5d815c02061776c6ef29ead1af741ac
Jim Garrett <jimgarrett123@gmail.com>
2023-06-22 10:14:22 -0400

    Update download_files.sh

--- download_files.sh
+++ download_files.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export PATH=$HOME/bin:$PATH
 echo Please provide your Scoarboard Server URL
-read serverUrl
-export SCOREBOARD_SERVER="$serverUrl"
+#read serverUrl
+export SCOREBOARD_SERVER="http://cs-workshop-scoreboard-server-scorecard-server.apps.jag2-aws.t0ij.p1.openshiftapps.com"
 mkdir $HOME/bin
 cd $HOME/bin
 wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz
