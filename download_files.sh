aa7f1dd50c2d20b028b591bf7655a5d83751f3ac
Jim Garrett <jimgarrett123@gmail.com>
2024-09-05 08:30:30 -0400

    Update download_files.sh

--- download_files.sh
+++ download_files.sh
@@ -9,8 +9,10 @@ wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/ope
 wget https://mirror.openshift.com/pub/openshift-v4/clients/rosa/latest/rosa-linux.tar.gz
 tar -xvf rosa-linux.tar.gz
 tar -xvf openshift-client-linux.tar.gz
+chmod +x create_vpc.sh
 cd $HOME
 sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 git clone https://github.com/openshift-cs/terraform-vpc-example
+./rosa_scripts/create_vpc.sjh
